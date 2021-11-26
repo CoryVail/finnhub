4f1c4672c5e4d8de49f047802f475f3b18c2c995
gnowak <gn9012@gmail.com>
2021-11-26 13:47:02 -0600

    refactor using function

--- main.py
+++ main.py
@@ -3,6 +3,34 @@ import csv
 import finnhub
 
 
+def create_csv(client, symbol, type):
+    # Get the financials.
+    financials = client.financials(symbol, type, 'quarterly')
+
+    # Isolate the column names from the Client response.
+    columns = []
+    for dict in financials['financials']:
+        for key, value in dict.items():
+            if key not in columns:
+                columns.append(key)
+    print(f"Columns: {columns}")
+
+    # Loop through financials and add to CSV.
+    data = []
+    for dict in financials['financials']:
+        values = []
+        for column in columns:
+            if column in dict:
+                values.append(dict[column])
+            else:
+                values.append('')
+        data.append(values)
+
+    with open(f"{symbol}_{type}.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+        writer.writerows(data)
+
 def main():
     # Get CLI args.
     parser = argparse.ArgumentParser()
@@ -19,87 +47,10 @@ def main():
 
     # Create a new Client object.
     c = finnhub.Client(api_key="sandbox_c620d3aad3iccpc4ang0")
-    # Get the financials.
-    financials = c.financials(symbol, 'cf','quarterly')
 
-    # Isolate the column names from the Client response.
-    columns = []
-    for dict in financials['financials']:
-        for key, value in dict.items():
-            if key not in columns:
-                columns.append(key)
-    print(f"Columns: {columns}")
-
-    # Loop through financials and add to CSV.
-    data = []
-    for dict in financials['financials']:
-        values = []
-        for column in columns:
-            if column in dict:
-                values.append(dict[column])
-            else:
-                values.append('')
-        data.append(values)
-    with open(f"{symbol}_cf.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(columns)
-        writer.writerows(data)
-        
-         # Create a new Client object for Income statement.
-    c = finnhub.Client(api_key="sandbox_c620d3aad3iccpc4ang0")
-    # Get the financials.
-    financials = c.financials(symbol, 'ic','quarterly')
-
-    # Isolate the column names from the Client response.
-    columns = []
-    for dict in financials['financials']:
-        for key, value in dict.items():
-            if key not in columns:
-                columns.append(key)
-    print(f"Columns: {columns}")
-
-    # Loop through financials and add to CSV.
-    data = []
-    for dict in financials['financials']:
-        values = []
-        for column in columns:
-            if column in dict:
-                values.append(dict[column])
-            else:
-                values.append('')
-        data.append(values)
-    with open(f"{symbol}_ic.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(columns)
-        writer.writerows(data)
-        
-         # Create a new Client object.
-    c = finnhub.Client(api_key="sandbox_c620d3aad3iccpc4ang0")
-    # Get the financials.
-    financials = c.financials(symbol, 'bs','quarterly')
-
-    # Isolate the column names from the Client response.
-    columns = []
-    for dict in financials['financials']:
-        for key, value in dict.items():
-            if key not in columns:
-                columns.append(key)
-    print(f"Columns: {columns}")
-
-    # Loop through financials and add to CSV.
-    data = []
-    for dict in financials['financials']:
-        values = []
-        for column in columns:
-            if column in dict:
-                values.append(dict[column])
-            else:
-                values.append('')
-        data.append(values)
-    with open(f"{symbol}_bs.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(columns)
-        writer.writerows(data)
+    create_csv(c, symbol, 'cf')
+    create_csv(c, symbol, 'ic')
+    create_csv(c, symbol, 'bs')
 
 if __name__ == "__main__":
     main()
